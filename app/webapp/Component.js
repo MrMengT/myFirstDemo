3677641e6a0aedc8a1f4b69d9624f37849354d14
MrMeng <mt5975@126.com>
2025-05-14 15:57:50 +0800

    can not load order/Component.js

---
+++ app/webapp/Component.js
@@ -0,0 +1,10 @@
+sap.ui.define(['sap/fe/core/AppComponent'], 
+    function(AppComponent) {
+    'use strict';
+
+    return AppComponent.extend('orders.Component', {
+        metadata: {
+            manifest: 'json'
+        }
+    });
+});
